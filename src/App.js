26855624974e485d18e37345807be7b35777b8a9
Abhinivesh <abhinivesh.smail@gmail.com>
2023-10-04 12:04:53 +0530

    6 challenges completed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function setCookie(name, value, days) {
   const cookieValue = `${name}=${value}; expires=${expirationDate.toUTCString()}; path=/`;
   document.cookie = cookieValue;
 }
-
+var a=10
 function App( ){
   console.log("ATCTF{b4c1c_F14G}")
    const handleContextMenu = (e) => {
@@ -16,6 +16,7 @@ function App( ){
   };
   setCookie("flag"," ATCTF{N1C3_C00k13}",23)
   fetch("https://api.npoint.io/a36172839e55efa040bc")
+  
  return(
     <div onContextMenu={handleContextMenu}>
     <Index />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+
+window.flag = "ATCTF{C0NS0L3_F4AG}";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
